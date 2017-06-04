0294a5a9ae66c41261cd9227c012d5baeb3646d3
blackanode <blackanode@gmail.com>
2017-06-04 17:25:26 -0600

    Changed how labels are added to grid

--- shuffleboard.py
+++ shuffleboard.py
@@ -1,5 +1,6 @@
 from Tkinter import Tk, Label, Button
 import serial
+from array import *
 
 
 def readSerial():
@@ -7,14 +8,22 @@ def readSerial():
         print "trying to read"
         c = ser.read()
         print c
-        
+        global p1scores
+        global p1totalInt
         if len(c) == 0:
-			break
+            break
         if c == "1":
-            print "in c ==1"
-            p1total.config(text='1') 
+			print "in c ==1"
+			
+			p1scores[0]+=1
+			p1totalInt += 1
+			p1round1.config(text = str(p1scores[0]))
+			p1total.config(text = str(p1totalInt))
         if c == '2':
-            p2total.config(text='2')
+			p1scores[0]+=2
+			p1totalInt += 2
+			p1round1.config(text = str(p1scores[0]))
+			p1total.config(text = str(p1totalInt))
     root.after(10, readSerial)
 							
 
@@ -31,35 +40,66 @@ round5 = Label(root, text = "Round 5",padx = 10).grid(row = 0, column = 5)
 round5 = Label(root, text = "Final",padx = 10).grid(row = 0, column = 6)
 #Player1
 player1 = Label(root, text="Player1").grid(row = 1)
-p1round1 = Label(root, text = "0").grid(row = 1, column = 1)
-p1round2 = Label(root, text = "0").grid(row = 1, column = 2)
-p1round3 = Label(root, text = "0").grid(row = 1, column = 3)
-p1round4 = Label(root, text = "0").grid(row = 1, column = 4)
-p1round5 = Label(root, text = "0").grid(row = 1, column = 5)
+
+p1round1 = Label(root, text = "0")
+p1round1.grid(row = 1, column = 1)
+
+p1round2 = Label(root, text = "0")
+p1round2.grid(row = 1, column = 2)
+
+p1round3 = Label(root, text = "0")
+p1round3.grid(row = 1, column = 3)
+
+p1round4 = Label(root, text = "0")
+p1round4.grid(row = 1, column = 4)
+
+p1round5 = Label(root, text = "0")
+p1round5.grid(row = 1, column = 5)
+
 p1total = Label(root, text = "0")
 p1total.grid(row = 1, column = 6)
+
 #Player2
 player2 = Label(root, text="Player2").grid(row = 2)
-p1round1 = Label(root, text = "0").grid(row = 2, column = 1)
-p2round2 = Label(root, text = "0").grid(row = 2, column = 2)
-p3round3 = Label(root, text = "0").grid(row = 2, column = 3)
-p4round4 = Label(root, text = "0").grid(row = 2, column = 4)
-p5round5 = Label(root, text = "0").grid(row = 2, column = 5)
+
+p1round2 = Label(root, text = "0")
+p1round2.grid(row = 2, column = 1)
+
+p2round2 = Label(root, text = "0")
+p2round2.grid(row = 2, column = 2)
+
+p3round3 = Label(root, text = "0")
+p3round3.grid(row = 2, column = 3)
+
+p4round4 = Label(root, text = "0")
+p4round4.grid(row = 2, column = 4)
+
+p5round5 = Label(root, text = "0")
+p5round5.grid(row = 2, column = 5)
+
 p2total = Label(root, text = "0")
 p2total.grid(row = 2, column = 6)
 
 #close function
 close_button = Button(root, text="Close", command=root.quit).grid(row = 3)
 
-
+#serial init
 serialPort = '/dev/ttyUSB0'
 baudRate = 9600
 ser = serial.Serial(serialPort, baudRate,timeout = 0, writeTimeout = 0)
-# 
+ 
 
-round = 0;
-player1Turn = True;
-player2Turn = False;
+round = 0
+throwcount = 0
+player1Turn = True
+player2Turn = False
+
+p1scores = array('i',[0,0,0,0,0])
+p2scores = array('i',[0,0,0,0,0])
+
+p1totalInt = 0
+p2totalInt = 0
+
 
 root.after(10, readSerial)
 root.mainloop()
